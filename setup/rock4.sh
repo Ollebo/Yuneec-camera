fae6d134ebce6742125c951d6bfd99a9cdaff705
Mattias Hemmingsson <matte@elino.se>
2023-01-26 14:40:57 +0100

    Update rock4.sh

--- setup/rock4.sh
+++ setup/rock4.sh
@@ -13,3 +13,7 @@ echo \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+
+got clone git@github.com:Ollebo/Yuneec-camera.git
+
